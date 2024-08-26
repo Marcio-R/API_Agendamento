73ccfded4cc8fb90e27b1e0c4d1e33f93e3053c1
marcio ronaldo alves <marcinho-ronald@hotmail.com>
2024-08-26 20:30:28 -0300

    Add project files.

---
+++ WebApplicationAPI/Data/WebApplicationAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationAPI.Entities;
+
+namespace WebApplicationAPI.Data
+{
+    public class WebApplicationAPIContext : DbContext
+    {
+        public WebApplicationAPIContext (DbContextOptions<WebApplicationAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contato> Contato { get; set; } = default!;
+    }
+}

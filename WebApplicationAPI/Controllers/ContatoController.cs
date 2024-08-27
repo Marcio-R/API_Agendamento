3d7a19c3fc10f7bc594ca7814382ff6e94ba1b74
marcio ronaldo alves <marcinho-ronald@hotmail.com>
2024-08-26 22:08:34 -0300

    Criado metodo inserir

---
+++ WebApplicationAPI/Controllers/ContatoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationAPI.Data;
+using WebApplicationAPI.Entities;
+
+namespace WebApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+        private readonly WebApplicationAPIContext _context;
+
+        public ContatoController(WebApplicationAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
